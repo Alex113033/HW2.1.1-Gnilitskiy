c8e45e2e090e26639ce5a06740006409ef66cb30
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-03-26 17:40:02 +0300

    Initial Commit

---
+++ HW2.1 Gnilitskiy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.1 Gnilitskiy
+//
+//  Created by Александр on 26.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
